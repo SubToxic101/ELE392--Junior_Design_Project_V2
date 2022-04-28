2bae74c97fcba13f465c09e320c280962e1573b6
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2022-04-28 13:10:05 -0400

    changes

--- Servo_Main/Servo_Main.ino
+++ Servo_Main/Servo_Main.ino
@@ -19,6 +19,16 @@ double setPoint = 0;
 double sensor1Pin = A0;
 double sensor2Pin = A1;
 
+double a3 = -0.00019;
+double a2 = -0.00058;
+double a1 = -0.74495;
+double a0 = 44.9389;
+
+double b3 = 0.000189;
+double b2 = -0.00058;
+double b1 = 0.74492;
+double b0 = 44.93859;
+
 double Kp1 = 0;
 double Ki1 = 0;
 double Kd1 = 0;
@@ -29,6 +39,14 @@ SharpDistSensor IRsensor1(sensor1Pin, 5);
 SharpDistSensor IRsensor2(sensor2Pin, 5);
 PID platform(&posInput, &anglOutput, &setPoint, Kp1, Ki1, Kd1, DIRECT);
 
+double angle_to_t1(double angle){
+  return a3*angle*angle*angle+a2*angle*angle+a1*angle+a0;
+}
+
+double angle_to_t2(double angle){
+  return b3*angle*angle*angle+b2*angle*angle+b1*angle+b0;
+}
+
 //Function for converting degrees to Usec.
 int returnUsec(int deg) {
   double uSec;
@@ -36,18 +54,29 @@ int returnUsec(int deg) {
   return (int)uSec;
 }
 
+
 double returnPos() {
   double pos = 0.0;
   //platform length
-  int len = 20;
+  int len = 320;
   int distance1 = IRsensor1.getDist();
   int distance2 = IRsensor2.getDist();
 
   if (distance1 < distance2) {
-    pos = (double)(-1*((abs(distance1-(len/2))+abs(distance2-(len/2)))/2));
+    if ((distance2 - distance1) < 30) {
+      pos = 0.0;
+    }
+    else {
+      pos = (double)(-1*((abs(distance1-(len/2))+abs(distance2-(len/2)))/2));
+    }
   }
   else if (distance1 > distance2) {
-    pos = (double)(1*((abs(distance1-(len/2))+abs(distance2-(len/2)))/2));
+    if ((distance1 - distance2) < 30) {
+      pos = 0.0;
+    }
+    else {
+      pos = (double)(1*((abs(distance1-(len/2))+abs(distance2-(len/2)))/2));
+    }
   }
   else {
     pos = 0.0;
@@ -64,14 +93,14 @@ void setup() {
   pwm.setOscillatorFrequency(25250000);
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
   IRsensor1.setModel(SharpDistSensor::GP2Y0A41SK0F_5V_DS);
-  IRsensor2.setModel(SharpDistSensor::GP2Y0A41SK0F_5V_DS)
+  IRsensor2.setModel(SharpDistSensor::GP2Y0A41SK0F_5V_DS);
 
   platform.SetSampleTime(50);
   platform.SetOutputLimits(-500, 500);
   platform.SetMode(AUTOMATIC);
   
   //set servos (arms) to 0 degrees
-  pw;m.writeMicroseconds(0,returnUsec(0));
+  pwm.writeMicroseconds(0,returnUsec(0));
   pwm.writeMicroseconds(1,returnUsec(180));
   delay(3000);
 }
@@ -79,6 +108,7 @@ void setup() {
 
 void loop() {
   //Loops to cycle servos through full range of motion
+  /*
   for (int i = 0; i < 180; i++) {
     servo0Out = i;
     servo1Out = i;
@@ -94,16 +124,18 @@ void loop() {
     pwm.writeMicroseconds(1,returnUsec(180 - servo1Out));
     delay(5);
   }
-  
-  posInput = returnPos();
+  */
+  //posInput = returnPos();
 
-  platform.Compute();
+  //platform.Compute();
   
   //Commands to write base to arm angle (one servo is reversed):
-  /*
+  
   servo0Out = 90;
   servo1Out = 90;
   pwm.writeMicroseconds(0,returnUsec(servo0Out));
   pwm.writeMicroseconds(1,returnUsec(180 - servo1Out));
-  */
+  Serial.println(angle_to_t1(0));
+  Serial.println(angle_to_t2(0));
+  
 }
