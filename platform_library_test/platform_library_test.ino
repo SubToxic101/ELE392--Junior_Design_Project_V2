3d6a12428c79284cc7383708d924f6fa9d46a0aa
Gabriel Ruoff <geruoff@gmail.com>
2022-05-02 14:27:06 -0400

    Platform library test

---
+++ platform_library_test/platform_library_test.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+#include <Platform.h>
+
+Servo Servo0;
+Servo Servo1;
+
+Platform platform;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+
+  Servo0.attach(A4);
+  Servo1.attach(A5);
+ 
+  platform._init(Servo0, Servo1, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//platform.setAngle(10);
+//delay(3000);
+//platform.setAngle(15);
+//delay(3000);
+//platform.setAngle(20);
+//delay(3000);
+}
